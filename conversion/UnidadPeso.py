c0c13dd9893ab81a4d1852f111570ed46d2f3c02
Cecilia Iribarren <cecirib@gmail.com>
2023-06-23 20:39:38 -0300

    Add:creamos la clase UnidadPeso

---
+++ conversion/UnidadPeso.py
@@ -0,0 +1,33 @@
+from conversion.ClaseConversor import ClaseConversor
+
+
+class UnidadPeso(ClaseConversor):
+
+    @classmethod
+    def kiloAGramo(self, valor):
+        self.valorAConvertir = valor
+        self.cuerpoOperacion = f'{valor} * 1000'
+        self.resultado = valor * 1000
+        return f'{valor} kilos son {valor * 1000} gramos'
+
+    @classmethod
+    def gramoAKilo(self, valor):
+        self.valorAConvertir = valor
+        self.cuerpoOperacion = f'{valor} / 1000'
+        self.resultado = valor / 1000
+        return f'{valor} gramos son {valor / 1000} kilos'
+
+    @classmethod
+    def kiloALibra(self, valor):
+        self.valorAConvertir = valor
+        self.cuerpoOperacion = f'{valor} * 2.205'
+        self.resultado = valor * 2.205
+        return f'{valor} kilos son {valor * 2.205} libras'
+
+    @classmethod
+    def libraAKilo(self, valor):
+        self.valorAConvertir = valor
+        self.cuerpoOperacion = f'{valor} / 2.205'
+        self.resultado = valor / 2.205
+        return f'{valor} libras son {valor / 2.205} kilos'
+    
