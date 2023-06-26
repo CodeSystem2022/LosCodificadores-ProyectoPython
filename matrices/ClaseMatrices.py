fc0e2d861b2b25493bbbd9e2139ec1a8ae275413
Cecilia Iribarren <cecirib@gmail.com>
2023-06-26 19:22:40 -0300

    Add: definimos los atributos de la clase Matrices y sus métodos get y set.

--- matrices/ClaseMatrices.py
+++ matrices/ClaseMatrices.py
@@ -1,4 +1,19 @@
 from Operacion import Operacion
 
 class Matrices(Operacion):
-    pass
+
+    def __init__(self):
+        self.matriz = None
+        self.matriz2 = None
+
+    def set_matriz(self, matriz):
+        self.matriz = matriz
+
+    def set_matriz2(self, matriz2):
+        self.matriz2 = matriz2
+
+    def get_matriz(self):
+        return self.matriz
+
+    def get_matriz2(self):
+        return self.matriz2
