d069531cf0a1e39e15d37ef0077a9658a32b0e46
Agustin Vera <ronedev25@gmail.com>
2023-06-29 21:50:57 -0300

    fix: Nombre de las clases

--- figuras/clase TrianguloBasico.py
+++
@@ -1,9 +0,0 @@
-from figuras.ClaseFigurasGeometricas import FigurasGeometricas
-
-
-class Rectangulo(FigurasGeometricas):
-
-    @classmethod
-    def area(cls):
-        return (cls.base * cls.altura) / 2
-
